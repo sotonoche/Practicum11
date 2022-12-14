37fd8c93d6b41b131cb92a86f8f5ffc0826b386c
Кирилл Кирсанов <willroger333@gmail.com>
2022-12-14 20:53:16 +0300

    Добавьте файлы проекта.

---
+++ Practicum11/Money.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Practicum11
+{
+    internal class Money
+    {
+        //поля класса
+        private int first;//номинал купюры
+        private int second;//количество купюр
+
+        //конструктор класса
+        public Money(int nomination, int value)
+        {
+            first = nomination;
+            second = value;
+        }
+
+        //методы класса
+        public void outInfo()
+        {
+            Console.WriteLine($"Номинал купюры - {first}");
+            Console.WriteLine($"Количество купюр - {second}");
+        }
+
+        public void isEnoughMoney(int sum)
+        {
+            if (first * second >= sum) Console.WriteLine("Денежной суммы хватит на покупку товара");
+            else Console.WriteLine("Денежной суммы не хватит на покупку товара");
+        }
+
+        public void quantityOfGoods(int price)
+        {
+            int sum = first * second;
+            
+            if (sum >= price)
+            {
+                Console.WriteLine($"Можно приобрести {sum/price} шт. товара");
+            }
+            else Console.WriteLine("Товар нельзя приобрести так как его цена превышает денежную сумму");
+        }
+
+        //свойства класса
+        public int First
+        {
+            get { return first; }
+            set { first = value; }
+        }
+        public int Second
+        {
+            get { return second; }
+            set { second = value; }
+        }
+
+        public int Sum
+        {
+            get { return first * second; }
+        }
+    }
+}
---
+++ Practicum11/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nomination, value, sum, price;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите номинал купюры: ");
+                    nomination = int.Parse(Console.ReadLine());
+                    Console.Write("Введите количество купюр: ");
+                    value = int.Parse(Console.ReadLine());
+                    Console.Write("Введите сумму закупки товара: ");
+                    sum = int.Parse(Console.ReadLine());
+                    Console.Write("Введите стоимость товара: ");
+                    price = int.Parse(Console.ReadLine());
+                    if (nomination <= 0 || value <= 0 || sum <= 0 || price <= 0) throw new Exception("Значение не может быть меньше или равно нулю!");
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введите корректное значение");
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }   
+            }
+            Console.WriteLine("------------------------------------------------------------");
+            Money money = new Money(nomination, value);
+
+            money.outInfo();
+            Console.WriteLine("------------------------------------------------------------");
+            money.isEnoughMoney(sum);
+            Console.WriteLine("------------------------------------------------------------");
+            money.quantityOfGoods(price);
+            Console.WriteLine("------------------------------------------------------------");
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите значение номинала: ");
+                    money.First = int.Parse(Console.ReadLine());
+                    Console.Write("Введите значение количества купюр: ");
+                    money.Second = int.Parse(Console.ReadLine());
+                    if (money.First <= 0 || money.Second <= 0) throw new Exception("Значение не может быть меньше или равно нулю!");
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введите корректное значение");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine($"Значение номинала равно {money.First}, значение кол-ва купюр равно {money.Second}\n" +
+                $"Сумма денег равна {money.Sum}");
+        }
+    }
+}
